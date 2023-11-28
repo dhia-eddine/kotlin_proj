e2c71062eb89293d048ed9e9b614c30d7b1cbfca
Dhia <diamandouj@gmail.com>
2023-11-28 11:47:49 +0100

    2

--- app/src/main/java/com/example/tplogin/MainActivity.kt
+++ app/src/main/java/com/example/tplogin/MainActivity.kt
@@ -3,18 +3,28 @@ package com.example.tplogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.tplogin.databinding.ActivityMainBinding
 import com.example.tplogin.databinding.ActivityRegistreBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 class MainActivity : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
     lateinit var user: FirebaseUser
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView:NavigationView
+    lateinit var actionToggle:ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initMenu(binding)
+        initNav(navView)
         auth = FirebaseAuth.getInstance()
         user = auth.currentUser!!
         if (user == null) {
@@ -29,6 +39,32 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
             finish()
+        }
+
+
+    }
+    private fun initMenu(binding: ActivityMainBinding) {
+        drawerLayout = binding.drawerLayout
+        navView = binding.designNavigationView
+
+        actionToggle = ActionBarDrawerToggle(this, drawerLayout, binding.toolbar, 0, 0)
+        drawerLayout.addDrawerListener(actionToggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+//make icon menu visible
+        actionToggle.syncState()
+    }
+
+    private fun initNav(navView: NavigationView, binding: ActivityMainBinding) {
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.users -> {true}
+                R.id.profile -> {true}
+                R.id.logout -> {true}
+
+                else -> {false}
+            }
         }
     }
 }
